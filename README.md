97d01049e43dbf54be13cbb6b3e7d2d82c9cf728
y-z-f <976691141@qq.com>
2016-04-12 17:45:33 +0800

    添加项目文件。

---
+++ README.md
@@ -0,0 +1,14 @@
+#简介
+具体使用方式可以见sample中的项目，后续会补全相关的API说明。
+
+##开发环境
+Visual Studio 2015 + Xamarin 4.0.0.1717
+
+##nuget安装
+    Install-Package Sino.Droid.ShapeLoading
+
+##参考
+https://github.com/zzz40500/android-shapeLoadingView
+
+#示例图片
+![](https://github.com/Xamarin-Cn/ShapeLoading/blob/master/img/1.gif)
---
+++ Sino.Droid.ShapeLoading/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sino.Droid.ShapeLoading")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sino")]
+[assembly: AssemblyProduct("Sino.Droid.ShapeLoading")]
+[assembly: AssemblyCopyright("Copyright © Sino 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
